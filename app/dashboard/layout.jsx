00fd1bec288798a9e1b764a87d6e630a3e5d66f7
Spark16x <spark2009971@gmail.com>
2024-10-11 01:28:58 +0530

    update layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
     
   })
   return (
-    user ? <SidebarProvider
+     <SidebarProvider
       style={
         {
           "--sidebar-width": "calc(var(--spacing) * 72)",
@@ -33,11 +33,11 @@ export default function RootLayout({ children }) {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-             {children}
+             {user ? children : <SigninBtn/>}
             </div>
           </div>
         </div>
       </SidebarInset>
-    </SidebarProvider> : <SigninBtn/>
+    </SidebarProvider> 
   );
 }
