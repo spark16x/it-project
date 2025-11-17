4c3ebd6c496ef62c88be89513c21064be1097671
spark16x <spark2009971@gmail.com>
2025-11-17 06:45:52 +0530

    update

---
+++ components/navWapper.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import Nav from "@/components/nav.jsx";
+
+export default function ClientNavWrapper() {
+  const pathname = usePathname();
+  
+  const hideNav =
+    pathname === "/login" ||
+    pathname === "/signup";
+  
+  return !hideNav && <Nav />;
+}
