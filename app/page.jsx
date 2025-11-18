895ae8afff71f5e0662142d6bb36a2ad95b6f66a
Spark16x <spark2009971@gmail.com>
2025-11-18 14:45:33 +0530

    update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,5 @@
 "use client";
-import { useState } from "react";
-import Link from "next/link"
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuIndicator,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
+
 
 export default function Home() {
 
