4c17eabdf3003940cf5f686803b4413ff13abd6a
spark16x <spark2009971@gmail.com>
2025-11-18 14:10:09 +0530

    update

---
+++ app/dashboard/layout.jsx
@@ -0,0 +1,37 @@
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import SplashScreen from "@/components/SplashScreen.jsx";
+import ClientNavWrapper from "@/components/navWrapper.jsx";
+
+export const metadata = {
+  title: "Edudel.lite",
+  description: "School mock website",
+  manifest: "/manifest.json",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta
+          name="google-site-verification"
+          content="7SnpTJD94MylkjVp3A2GY9wDF1Xhvs1yLkq_AH7nJxQ"
+        />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+      </head>
+
+      <body className="w-full h-screen">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <SplashScreen />
+
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
