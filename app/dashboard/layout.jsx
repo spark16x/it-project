60a81fbdb0faca8e6d7b227bda74e18fabc30103
Spark16x <spark2009971@gmail.com>
2025-11-19 19:18:43 +0530

    update layout.jsx

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -11,8 +11,7 @@ import {
 export default function RootLayout({ children }) {
   return (
     
- 
-    <SidebarProvider
+     <SidebarProvider
       style={
         {
           "--sidebar-width": "calc(var(--spacing) * 72)",
@@ -20,18 +19,18 @@ export default function RootLayout({ children }) {
         }
       }
     >
-      <SidebarInset>
       <AppSidebar variant="inset" />
-              <SiteHeader /> 
-              <div className = "flex flex-1 flex-col" >
+      <SidebarInset>
+        <SiteHeader />
+        <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          {children}
-               </div> 
-             </div> 
+             {children}
             </div>
-            </SidebarInset>
-          </SidebarProvider>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
     
   );
 }
