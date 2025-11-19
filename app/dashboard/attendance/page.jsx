62998f98ed551e5a14e4aff05cf15c5172d56121
Spark16x <spark2009971@gmail.com>
2025-11-19 14:42:47 +0530

    update page.jsx

--- app/dashboard/attendance/page.jsx
+++ app/dashboard/attendance/page.jsx
@@ -1,9 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
+
 
 export default function AttendancePage() {
   // Fake student data
@@ -18,48 +16,6 @@ export default function AttendancePage() {
     { id: 8, name: "Sanya Gupta", roll: 8 },
   ]
 
-
-  // return (
-  //   <div className="max-w-3xl mx-auto py-10">
-  //     <Card className="shadow-[0_0_20px] shadow-blue-500/40">
-  //       <CardHeader>
-  //         <CardTitle className="text-center text-xl">
-  //           📋 Class Attendance
-  //         </CardTitle>
-  //       </CardHeader>
-
-  //       <CardContent className="space-y-4">
-  //         {/* Table-like UI */}
-  //         <div className="grid grid-cols-3 font-medium border-b pb-2">
-  //           <p>Roll</p>
-  //           <p>Name</p>
-  //           <p>Status</p>
-  //         </div>
-
-  //         {students.map((student) => (
-  //           <div
-  //             key={student.id}
-  //             className="grid grid-cols-3 items-center py-2 border-b"
-  //           >
-  //             <p>{student.roll}</p>
-  //             <p>{student.name}</p>
-  //             <Checkbox
-  //               checked={attendance[student.id] || false}
-  //               onCheckedChange={() => toggleAttendance(student.id)}
-  //             />
-  //           </div>
-  //         ))}
-
-  //         <Button
-  //           onClick={handleSubmit}
-  //           className="w-full mt-4"
-  //         >
-  //           Save Attendance
-  //         </Button>
-  //       </CardContent>
-  //     </Card>
-  //   </div>
-  // )
   return (
     <div>attendance</div>
   )
