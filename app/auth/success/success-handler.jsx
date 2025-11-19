adc1d96244c979677e92edb0316ce9e4a1125e3b
Spark16x <spark2009971@gmail.com>
2025-11-19 19:46:48 +0530

    update success-handler.jsx

--- app/auth/success/success-handler.jsx
+++ app/auth/success/success-handler.jsx
@@ -2,16 +2,21 @@
 
 import { useSearchParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
+import { useToast } from "@/hooks/use-toast"
 
 export default function SuccessHandler() {
   const params = useSearchParams();
   const router = useRouter();
+  const { toast } = useToast()
   
   useEffect(() => {
     const token = params.get("token");
     
     if (token) {
       document.cookie = `token=${token}; path=/; max-age=604800; Secure; SameSite=Lax`;
+      toast({
+        title: "Login is successfull"
+      })
       router.replace("/dashboard");
     }
   }, [params, router]);
