a59f6914d580a827c5556243a80450fd66f1ce38
Spark16x <spark2009971@gmail.com>
2025-11-19 15:41:30 +0530

    update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,19 +1,27 @@
+import { AppSidebar } from '@/components/app-sidebar'
 import { ChartAreaInteractive } from '@/components/chart-area-interactive'
 import { DataTable } from '@/components/data-table'
 import { SectionCards } from '@/components/section-cards'
 import { SiteHeader } from '@/components/site-header'
 import {
   SidebarInset,
+  SidebarProvider,
 } from '@/components/ui/sidebar'
 
 import data from "./data.json"
 
-// Force this page to be rendered dynamically (avoid build-time prerender)
-
-
 export default function Page() {
   return (
-    <SidebarInset>
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        }
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
         <SiteHeader />
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
@@ -22,9 +30,11 @@ export default function Page() {
               <div className="px-4 lg:px-6">
                 <ChartAreaInteractive />
               </div>
+              <DataTable data={data} />
             </div>
           </div>
         </div>
       </SidebarInset>
+    </SidebarProvider>
   )
 }
