0acb5cb509801fb4c0e86f2a7f2f712f7a345ab2
Spark16x <spark2009971@gmail.com>
2025-11-30 14:43:33 +0530

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import SplashScreen from "@/components/SplashScreen.jsx";
 import ClientNavWrapper from "@/components/navWrapper.jsx";
 import { Toaster } from "@/components/ui/toaster"
+import NextTopLoader from 'nextjs-toploader';
 
 export const metadata = {
   title: "Edudel.lite",
@@ -27,6 +28,19 @@ export default function RootLayout({ children }) {
           enableSystem
           disableTransitionOnChange
         >
+          <NextTopLoader
+            color="#2299DD"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            easing="ease"
+            speed={200}
+            template='<div class="bar" role="bar"><div class="peg"></div></div> 
+              <div class="spinner" role="spinner"><div class="spinner-icon"></div></div>'
+            zIndex={1600}
+            showAtBottom={false}
+                />
           <SplashScreen />
           {/* client component handles pathname + hiding nav */}
           {children}
