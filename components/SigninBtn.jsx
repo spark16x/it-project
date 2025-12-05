369d7b887a1a32d4c4edac327154177850866fee
Spark16x <spark2009971@gmail.com>
2025-12-05 07:47:52 +0530

    update SigninBtn.jsx

--- components/SigninBtn.jsx
+++ components/SigninBtn.jsx
@@ -6,7 +6,7 @@ export default function SigninBtn() {
   const router = useRouter()
 
   return (
-  <div className="flex flex-col gap-4 py-4 items-center justify-center">
+  <div className="h-full flex flex-col gap-4 py-4 items-center justify-center">
     <Button type="submit" className="w-md" onClick={()=> router.push("/login")
  }>Login</Button>
  </div>
